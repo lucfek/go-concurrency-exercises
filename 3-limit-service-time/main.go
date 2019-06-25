655a78b4c604a15a0ff42ea705f718561b44b2fc
Lucjan Osuch <ljo@ges.sh>
2019-06-25 14:13:51 +0200

    Add Limit Service Time for Free-tier Users funcionality

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,10 @@
 
 package main
 
+import (
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -18,10 +22,31 @@ type User struct {
 	TimeUsed  int64 // in seconds
 }
 
-// HandleRequest runs the processes requested by users. Returns false
-// if process had to be killed
+func timeCounter(u *User, procDone *bool, killed chan<- bool) {
+	for {
+		time.Sleep(time.Second)
+		u.TimeUsed++
+		if *procDone {
+			if u.TimeUsed > 10 && !u.IsPremium {
+				killed <- true
+			}
+			killed <- false
+			break
+		}
+	}
+}
 func HandleRequest(process func(), u *User) bool {
+	procDone := false
+	killed := make(chan bool, 1)
+	go timeCounter(u, &procDone, killed)
+	if u.TimeUsed > 10 && !u.IsPremium {
+		return false
+	}
 	process()
+	procDone = true
+	if <-killed {
+		return false
+	}
 	return true
 }
 
